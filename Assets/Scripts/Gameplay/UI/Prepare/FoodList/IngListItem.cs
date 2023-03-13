cb387aa75d37f348c31ea869332f529f16c9321b
Mikhail Dunaev <mih.dunaev@gmail.com>
2023-03-14 00:17:38 +0100

    Add UI

---
+++ Assets/Scripts/Gameplay/UI/Prepare/FoodList/IngListItem.cs
@@ -0,0 +1,27 @@
+﻿using Data.Ingredients;
+using Gameplay.Controllers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Gameplay.UI.Prepare.FoodList
+{
+    public class IngListItem : MonoBehaviour, IPointerDownHandler
+    {
+        [SerializeField] private Image _image;
+
+        private IngredientData _data;
+        private FoodController FoodController => GameplayController.Instance.FoodController;
+        
+        public void SetIngredient(IngredientData ingredient)
+        {
+            _data = ingredient;
+            _image.sprite = ingredient.Sprite;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            FoodController.SelectIngredient(_data);
+        }
+    }
+}
